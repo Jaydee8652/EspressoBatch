8f98b927ffcd1378f0104f980caf06b774a0ed82
Jaydee8652 <134213634+Jaydee8652@users.noreply.github.com>
2026-07-13 16:38:26 -0600

    Add files via upload

--- utils/post_processing.py
+++ utils/post_processing.py
@@ -3,7 +3,7 @@
 # Extracts desired data from PWSCF and gipaw .out files to a summary file, saved here and in a dedicated directory
 # Automatically removes symmetry equivalent atoms from the gipaw output
 
-# Automatically run by slurm within the input directory, takes the home directory as command line input
+# Automatically run by slurm within the input directory
 
 # All processes are reported to post_processing.log for debugging, all post_processing scripts output to the same log within a home directory,
 # and individual processes produce their own log locally within the input file
@@ -27,7 +27,8 @@ def printToLog(info):#Prints and logs in one, convention I personally like
     logs = os.path.join(homeDirectory, "logs")
     if not os.path.exists(logs):
         os.makedirs(logs)
-    
+    info = str(info)
+
     time = ""
     if not info.startswith(" ---"):
         time = str(datetime.datetime.now().strftime("[%H:%M:%S] "))
@@ -50,6 +51,8 @@ def removeDirectory(path, text):
     if os.path.exists(path):
         printToLog(text + " ["+ path + "]")
         shutil.rmtree(path)
+
+CIF_symmetryElements = []
 
 PWSCF_ecutwfc = ""
 PWSCF_ecutrho = "" 
@@ -77,9 +80,6 @@ GIPAW_msCorrection = []
 
 GIPAW_done = False
 
-CIF_symmetryEquivalents = 0
-CIF_inversionCentre = False
-CIF_symmetryFactor = ""
 
 #Main
 log = str(os.path.basename(sys.argv[0]).split(".")[0]+".log")
@@ -97,29 +97,15 @@ createDirectory(summariesPath, "# INFO - No directory found for summary files, c
 outputsPath = os.path.join(homeDirectory, "Output_Files")
 createDirectory(outputsPath, "# INFO - No directory found for output files, created at", False)
 
-# Get .cif directory
+# Get .cif
 cifPath = os.path.join(refcodeDirectory, refcode+".cif")
 if os.path.isfile(cifPath):
     with open(cifPath, "r") as cif:
-        for line in cif:
+        for line in cif: 
             if line[0].isdigit():
-                CIF_symmetryEquivalents += 1
-                if "-x,-y,-z" in line:
-                    CIF_inversionCentre = True
+                CIF_symmetryElements.append(line.split()[1])     
 else:
     printToLog("# WARN - Compound ["+refcode+"] does not have a .cif file")
-    quit()
-
-# Get structure data .csv
-structureDataPath = os.path.join(homeDirectory, "structure_data.csv")
-if os.path.exists(structureDataPath):
-    df = pd.read_csv(structureDataPath, encoding="utf-8-sig")
-    df.set_index('[REFCODE]', inplace = True)
-    cellVolume = float(df.at[refcode, "[_cell_volume]"])
-    rcellVolume = float(df.at[refcode, "[_rcell_volume]"])
-    CIF_symmetryFactor = round(float(CIF_symmetryEquivalents) / round(cellVolume / rcellVolume))
-else:
-    printToLog("# WARN - No .csv file found to load compound data. Copy .csv from the CSD to the following path ["+ structureDataPath + "]")
     quit()
 
 # Remove .save directory
@@ -143,18 +129,28 @@ if os.path.exists(summaryPath):
 printToLog("# INFO - Compound ["+ refcode +"] Populating summary file ["+ summaryPath + "]")
 with open(summaryPath, "a") as summary:
     print("#Output summary for compound with refcode ["+refcode+"]", file=summary)
-    print("\n# -CIF data\n", file=summary)
-    print("CIF_symmetryEquivalents = "+str(CIF_symmetryEquivalents), file=summary)
-    print("CIF_symmetryFactor = "+str(CIF_symmetryFactor), file=summary)
-    print("CIF_inversionCentre = "+str(CIF_inversionCentre), file=summary)
     
-    #REFCODE.in
-    pwscfIn = os.path.join(refcodeDirectory, refcode+".in")
-    if os.path.isfile(pwscfIn):
-        print("\n# -PWSCF params-\n", file=summary)            
-        with open(pwscfIn) as file:
+    #REFCODE_batch.txt
+    batch = os.path.join(refcodeDirectory, refcode+"_batch.txt")
+    if os.path.isfile(batch):
+        with open(batch) as file:
             lines = file.read().splitlines()
             for line in lines:
+                print(line, file=summary)
+    else:
+        print("# WARN - No batch file found for compound with refcode ["+refcode+"]", file=summary)
+        printToLog("# WARN - Compound ["+refcode+"] No batch file found")
+
+    # Get .in
+    inPath = os.path.join(refcodeDirectory, refcode+".in")
+    if os.path.isfile(inPath):
+        print("\n# -PWSCF params-\n", file=summary)            
+        with open(inPath, "r") as file:
+            start = 0
+            end = 0
+    
+            lines = file.readlines()
+            for number, line in enumerate(lines, 1):  
                 if "ecutwfc" in line:
                     PWSCF_ecutwfc = float(re.sub("[^0-9.]", "", line).strip())
 
@@ -172,11 +168,42 @@ with open(summaryPath, "a") as summary:
                 elif "conv_thr" in line:
                     PWSCF_conv_thr = line[line.find("=")+1:].strip()
 
-                    print("PWSCF_conv_thr = "+str(PWSCF_conv_thr), file=summary)
-    else:
-        print("# WARN - No .in file found for compound with refcode ["+refcode+"]", file=summary)
-        printToLog("# WARN - Compound ["+refcode+"] No .in file found")
+                    print("PWSCF_conv_thr = "+str(PWSCF_conv_thr), file=summary)            
     
+                elif "ATOMIC_POSITIONS" in line:  
+                    start = number
+                elif "K_POINTS automatic" in line:  
+                    end = number - 3
+            sub = lines[start:end]
+            
+            curr = 0
+            symmetryEquivelents = []
+            for number, line in enumerate(sub, 0):
+                if number == curr:
+                    equivalents = []
+                    coordinates = re.sub('\s{2,}', ' ', line).strip().split(" ")
+                    
+                    x = float(coordinates[1])
+                    y = float(coordinates[2])
+                    z = float(coordinates[3])
+    
+                    for operation in CIF_symmetryElements:
+                        operation = operation.split(",")
+    
+                        equivalent = []
+                        equivalent.append(round(float(eval(str(operation[0]))), 5) % 1)
+                        equivalent.append(round(float(eval(str(operation[1]))), 5) % 1)
+                        equivalent.append(round(float(eval(str(operation[2]))), 5) % 1)      
+                        
+                        equivalents.append(str(equivalent))
+                        
+                    unique = len(list(set(equivalents))) 
+                    symmetryEquivelents.append(unique)
+                    curr += unique            
+    else:
+        printToLog("# WARN - Compound ["+refcode+"] does not have a .in file")
+        quit()
+
     #REFCODE.out
     pwscfOut = os.path.join(refcodeDirectory, refcode+".out")
     if os.path.isfile(pwscfOut):
@@ -185,7 +212,7 @@ with open(summaryPath, "a") as summary:
 
             lineNumber = 0
             read = file.read()
-            lines = read.splitlines()
+            lines = file.readlines()
 
             for line in lines:
                 lineNumber += 1
@@ -259,15 +286,13 @@ with open(summaryPath, "a") as summary:
         
     #gipaw.REFCODE.out
     gipawOut = os.path.join(refcodeDirectory, "gipaw."+refcode+".out")
-    sigmaStart = 0
     if os.path.isfile(gipawOut):
+
         print("\n# -GIPAW output-\n", file=summary)            
         with open(gipawOut) as file:
-            lineNumber = 0
-            lines = file.read().splitlines()
-            for line in lines:
-                lineNumber += 1
-                
+            start = 0
+            lines = file.readlines()
+            for number, line in enumerate(lines, 0): 
                 if "Program GIPAW" in line:
                     year = re.sub('\s{2,}', ' ', line.strip()).strip().split(" ")[5]
                     if len(year) < 9:
@@ -296,62 +321,15 @@ with open(summaryPath, "a") as summary:
 
                     print("GIPAW_mscPPM = "+str(GIPAW_mscPPM), file=summary)
                 elif "NMR macroscopic correction" in line:
-                    GIPAW_msCorrection.append(re.sub('\s{2,}', ' ', lines[lineNumber]).strip().split(" "))
-                    GIPAW_msCorrection.append(re.sub('\s{2,}', ' ', lines[lineNumber+1]).strip().split(" "))
-                    GIPAW_msCorrection.append(re.sub('\s{2,}', ' ', lines[lineNumber+2]).strip().split(" "))
+                    GIPAW_msCorrection.append(re.sub('\s{2,}', ' ', lines[number+1]).strip().split(" "))
+                    GIPAW_msCorrection.append(re.sub('\s{2,}', ' ', lines[number+2]).strip().split(" "))
+                    GIPAW_msCorrection.append(re.sub('\s{2,}', ' ', lines[number+3]).strip().split(" "))
 
                     GIPAW_msCorrection = str(GIPAW_msCorrection)
                     print("GIPAW_msCorrection = "+str(GIPAW_msCorrection), file=summary)
                 elif "Total sigma" in line:
-                    if sigmaStart == 0:
-                        sigmaStart = lineNumber
-                    
-                    
-                    if ((lineNumber - sigmaStart) / 10) % CIF_symmetryFactor == 0:
-                        regex = re.compile('[^a-zA-Z ]')
-                        initialAtom = str(line.strip()[:13].strip())
-                        initialElement = regex.sub('', initialAtom).strip().split(" ")[-1]
-                        
-                        previousDict = {}                        
-                        for i in range(CIF_symmetryFactor):
-                            curr = lines[(10 * i) + lineNumber - 1]
-
-                            atom = str(curr.strip()[:13].strip())
-                            sigma = float(curr.strip()[-15:].lstrip())
-                            previousDict[atom] = sigma
-
-                            position = re.sub('\s{2,}', ' ', curr[curr.find("(")+1:curr.find(")")]).strip().split(" ")
-                            if CIF_inversionCentre and position == ['0.000000', '0.000000', '0.000000']:
-                                printToLog("# WARN - Compound ["+refcode+"] Atom on inversion centre ["+str(atom)+"] ["+str(position)+"]")
-                                previousDict.clear()
-                                break
-
-                        if len(previousDict) == 0:
-                            for i in range(round(CIF_symmetryFactor / 2)):
-                                curr = lines[(10 * i) + lineNumber - 1]
-    
-                                atom = str(curr.strip()[:13].strip())
-                                sigma = float(curr.strip()[-15:].lstrip())
-                                previousDict[atom] = sigma
-                                sigmaStart -= 10
-                        
-                        workingDict = previousDict.copy()
-                        for atom in previousDict.keys():
-                            element = regex.sub('', atom).strip().split(" ")[-1]
-                            
-                            if not element == initialElement and atom in workingDict:
-                                del workingDict[atom]
-                                sigmaStart -= 10
-                                printToLog("# WARN - Compound ["+refcode+"] Symmetry equivalent atom of different type ["+str(initialAtom)+"] and ["+str(atom)+"]")
-
-                        currentSum = 0
-                        for activeAtom, activeSigma in workingDict.items():
-                            currentSum += activeSigma
-                            for atom, sigma in workingDict.items():
-                                diff = activeSigma - sigma
-                                if diff < -tolerance or diff > tolerance:
-                                    printToLog("# WARN - Compound ["+refcode+"] has symmetry equivalent atoms ["+str(activeAtom)+"] and ["+str(atom)+"] outside tolerance ["+str(diff)+"]")
-                        print("_"+ str(line.lstrip()) + " - Averaged over "+str(len(workingDict))+" atoms: "+str(currentSum / len(workingDict)), file=summary)
+                    if start == 0:
+                        start = number                
                 elif "JOB DONE" in line:
                     GIPAW_done = True
                     print("GIPAW_done = "+str(GIPAW_done), file=summary)
@@ -359,6 +337,45 @@ with open(summaryPath, "a") as summary:
                 print("GIPAW_done = "+str(GIPAW_done), file=summary) 
                 print("# WARN - GIPAW did not run to completion", file=summary) 
                 printToLog("# WARN - Compound ["+refcode+"] GIPAW did not run to completion")
+
+            sub = lines[start:]
+            count = 0
+            previous = -10
+            for number, line in enumerate(sub, 0):
+                if count < len(symmetryEquivelents):
+                    
+                    if number == previous + (10 * (symmetryEquivelents[count])):
+                        printToLog("# INFO - Expecting ["+str(symmetryEquivelents[count])+"] symmetry equivalent atoms")
+                        regex = re.compile('[^a-zA-Z ]')
+    
+                        previous = {}
+                        for i in range(symmetryEquivelents[count]):
+                            temp = number - (10 * i)
+                            printToLog("# INFO - "+str(sub[temp]).strip())
+                            curr = sub[temp]
+    
+                            atom = str(curr.strip()[:13].strip())
+                            sigma = float(curr.strip()[-15:].lstrip())
+                            
+                            previous[atom] = sigma
+    
+                        currentSum = 0
+                        for activeAtom, activeSigma in previous.items():
+                            currentSum += activeSigma
+                            for atom, sigma in previous.items():
+                                diff = activeSigma - sigma
+                                if diff < -tolerance or diff > tolerance:
+                                    printToLog("# WARN - Compound ["+refcode+"] has symmetry equivalent atoms ["+str(activeAtom)+"] and ["+str(atom)+"] outside tolerance ["+str(diff)+"]")
+                        matrix = []
+                        matrix.append(re.sub('\s{2,}', ' ', sub[number+1]).strip().split(" "))
+                        matrix.append(re.sub('\s{2,}', ' ', sub[number+2]).strip().split(" "))
+                        matrix.append(re.sub('\s{2,}', ' ', sub[number+3]).strip().split(" "))
+
+                        matrix = str(matrix)
+                        print("_"+ str(line.lstrip().strip()) + " - Averaged over "+str(len(previous))+" atoms: "+str(round(currentSum / len(previous),2)) + " "+ matrix, file=summary)
+                        
+                        count += 1
+                        previous = number
     else:
         print("# WARN - No gipaw .out file found for compound with refcode ["+refcode+"]", file=summary)
         printToLog("# WARN - Compound ["+refcode+"] No GIPAW .out file found")
@@ -377,6 +394,6 @@ shutil.copyfile(summaryPath, summaryCopyPath)
 printToLog("# INFO - Compound ["+ refcode +"] Copied summary file ["+ summaryCopyPath + "]")
 
 outputPath = os.path.join(outputsPath, refcode)
-removeDirectory(outputPath, "# INFO - Cleaning existing output path at")
+removeDirectory(outputPath, "# INFO - Compound ["+ refcode +"] Cleaning existing output path at")
 printToLog("# INFO - Compound ["+ refcode +"] Copied output path ["+str(refcodeDirectory)+"] to ["+str(outputPath)+"]")
 shutil.copytree(refcodeDirectory, outputPath)
